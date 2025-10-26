47f13724c8214f262102bea48f4bba9bfc47e0b0
deguzmanjasperrey-beep <deguzmanjasperrey@isufst.edu.ph>
2025-10-25 22:11:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,9 @@
-from flask import Flask, jsonify, request
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to my Flask API!"
-
-@app.route('/student/<name>')
-def get_student_details(name):
-    # Sample student data
-    students = {
-        "jasper": {
-            "name": "Jasper Rey de Guzman",
-            "grade": bsit3,
-            "section": "Stallman",
-            "height": 165,  # in cm
-            "weight": 55    # in kg
-        },
-        "maria": {
-            "name": "jedidiah defacto",
-            "grade": bsit3,
-            "section": "Stallman",
-            "height": 158,
-            "weight": 50
-        }
-    }
-    
-    student = students.get(name.lower())
-    if student:
-        return jsonify(student)
-    else:
-        return jsonify({"error": "Student not found"}), 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/student/profile')
+def get_student_profile():
+    return jsonify({
+        "name": "Jasper Rey de Guzman",
+        "grade": 10,
+        "section": "Zechariah",
+        "height": "5'7\"",
+        "weight": "65 kg"
+    })
